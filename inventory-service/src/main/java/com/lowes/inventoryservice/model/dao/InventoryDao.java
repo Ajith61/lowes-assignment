5a003b945bfbf3d8dbf826719a37b16f26edf121
Ajith <ajithz2307@gmail.com>
2022-06-19 21:32:25 +0530

    index added

--- inventory-service/src/main/java/com/lowes/inventoryservice/model/dao/InventoryDao.java
+++ inventory-service/src/main/java/com/lowes/inventoryservice/model/dao/InventoryDao.java
@@ -1,10 +1,10 @@
 package com.lowes.inventoryservice.model.dao;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -12,27 +12,32 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 public class InventoryDao {
 	@Id
 	private String id;
+	@Indexed(unique = true)
 	private String productId;
 	private long stockCount;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getProductId() {
 		return productId;
 	}
+
 	public void setProductId(String productId) {
 		this.productId = productId;
 	}
+
 	public long getStockCount() {
 		return stockCount;
 	}
+
 	public void setStockCount(long stockCount) {
 		this.stockCount = stockCount;
 	}
-
-
 
 }
--- product-service/src/main/java/com/lowes/productservice/model/dao/ProductDao.java
+++ product-service/src/main/java/com/lowes/productservice/model/dao/ProductDao.java
@@ -1,20 +1,26 @@
 package com.lowes.productservice.model.dao;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "product_master")
-public class ProductDao{
+public class ProductDao {
 	@Id
 	private String id;
+	@Indexed(unique = true)
 	private String productId;
 	private String productName;
 	private long productPrice;
 	private int productStock;
+
 	public String getId() {
 		return id;
 	}
-	public ProductDao(){}
+
+	public ProductDao() {
+	}
+
 	public ProductDao(String id, String productId, String productName, long productPrice, int productStock) {
 		super();
 		this.id = id;
@@ -23,34 +29,41 @@ public class ProductDao{
 		this.productPrice = productPrice;
 		this.productStock = productStock;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getProductId() {
 		return productId;
 	}
+
 	public void setProductId(String productId) {
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public int getProductStock() {
 		return productStock;
 	}
+
 	public void setProductStock(int productStock) {
 		this.productStock = productStock;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public long getProductPrice() {
 		return productPrice;
 	}
+
 	public void setProductPrice(int productPrice) {
 		this.productPrice = productPrice;
 	}
-
-
 
 }
